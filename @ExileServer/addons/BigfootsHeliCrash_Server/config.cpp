de4764f98530d6ce1760e75d18915c3fc4310d41
skosko <skonemesis@gmail.com>
2025-02-21 15:43:06 -0800

    Updated BigfootsHeliCrash_Server config.cpp with corrected preInit and postInit paths

--- @ExileServer/addons/BigfootsHeliCrash_Server/config.cpp
+++ @ExileServer/addons/BigfootsHeliCrash_Server/config.cpp
@@ -1,9 +1,69 @@
 /*
  * Bigfoot's Heli Crash - Server Configuration
+ *
+ * 	Updated by: sko & Ghost PGM DEV TEAM
+ * 
+ * This file defines the required dependencies and initializes key functions for the Heli Crash spawner.
+*/
+
+class CfgPatches
+{
+	class BigfootsHeliCrash_Server
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"exile_client", "exile_server"};
+	};
+};
+
+class CfgFunctions
+{
+	class BigfootsHeliCrash
+	{
+		class bootstrap
+		{
+			file = "\BigfootsHeliCrash_Server\bootstrap";
+			
+			class ExileServer_BigfootsHeliCrash_preInit
+			{
+				preInit = 1;
+				file = "\BigfootsHeliCrash_Server\bootstrap\fn_preInit.sqf";
+			};
+			
+			class ExileServer_BigfootsHeliCrash_postInit
+			{
+				postInit = 1;
+				file = "\BigfootsHeliCrash_Server\bootstrap\fn_postInit.sqf";
+			};
+		};
+
+		class code
+		{
+			file = "\BigfootsHeliCrash_Server\code";
+			class ExileServer_BigfootsHeliCrash_addItemsToCrateCommand{};
+			class ExileServer_BigfootsHeliCrash_addMoneyToCrateCommand{};
+			class ExileServer_BigfootsHeliCrash_createHeliCrashMarkerCommand{};
+			class ExileServer_BigfootsHeliCrash_getWreckIdForSpawnCountIndexQuery{};
+			class ExileServer_BigfootsHeliCrash_initialize{};
+			class ExileServer_BigfootsHeliCrash_maintainHeliCrashCommand{};
+			class ExileServer_BigfootsHeliCrash_manageAIPatrols{};
+			class ExileServer_BigfootsHeliCrash_sendClientNotificationCommand{};
+			class ExileServer_BigfootsHeliCrash_setupCrateCommand{};
+			class ExileServer_BigfootsHeliCrash_spawnAIReinforcements{};
+			class ExileServer_BigfootsHeliCrash_spawnHeliCrashCommand{};
+			class ExileServer_BigfootsHeliCrash_spawnSupportAssets{};
+			class ExileServer_BigfootsHeliCrash_util_logCommand{};
+		};
+	};
+};
+
+/* testing above
+ * Bigfoot's Heli Crash - Server Configuration
  * Updated by: sko & Ghost PGM DEV TEAM
  * 
  * This file defines the required dependencies and initializes key functions for the Heli Crash spawner.
- */
+
 
 class CfgPatches
 {
@@ -34,3 +94,4 @@ class CfgFunctions
         };
     };
 };
+*/
